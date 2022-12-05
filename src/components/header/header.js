2a024d941806c409e8f8955ca8aa8e86adf17be0
VPolazau <VPolazau@gmail.com>
2022-12-05 14:27:43 +0300

    add filter

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
+import {btnEvent} from '../../events/event'
+
 import './header.scss'
 
 const Header = () => {
+
+  const onFiltered = event => {
+    btnEvent.emit('onFilterChange', event.target.value)
+  }
+
+  const newItem = () => {
+    btnEvent.emit('onAddNewItem')
+    console.log('onAddNewItem')
+  }
+
   return (
     <div className='Header'>
       <h1 className='Header__title'>Test Notes</h1>
-      <input className='Header__input' type='text' placeholder='enter tag' />
-      <button className='btn Header__btn-new'>New</button>
+      <input
+        className='Header__input'
+        type='text'
+        placeholder='enter tag'
+        onChange={onFiltered}
+      />
+      <button className='btn Header__btn-new' onClick={newItem}>New</button>
     </div>
   )
 }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useSelector } from 'react-redux'
+import { btnEvent } from '../../events/event'
 
 import Item from '../item'
 
 import './item-list.scss'
 
 const ItemList = () => {
-  const { notes } = useSelector(data => data.notes.notes)
+  const { notes } = useSelector(data => data.notes)
+  const [filteredNotes, setFilteredNotes] = useState(notes)
+
+  useEffect(() => {
+    btnEvent.addListener('onFilterChange', (term) => {
+      setFilteredNotes( () => {
+        return notes.filter(note => note.tag.join().indexOf(term) > -1)
+      })
+    })
+    
+    return () => {
+      btnEvent.removeAllListeners()
+    }
+  },[filteredNotes])
+  
   return (
     <div className='ItemList'>
-      {notes.map(item => (
+      {filteredNotes.map(item => (
         <Item key={item.id} info={item} />
       ))}
     </div>
