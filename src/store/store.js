6b748da52e839202df2938579b9c44b9d8b637fb
VPolazau <VPolazau@gmail.com>
2022-12-05 14:30:33 +0300

    refactor store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,8 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import notesReducer from './redusers/notesSlice'
-
-const rootReducer = combineReducers({
-  notes: notesReducer,
-})
 
 export const store = configureStore({
   reducer: {
-    notes: rootReducer,
+    notes: notesReducer,
   }
 })
